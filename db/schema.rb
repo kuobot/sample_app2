b82861e6a32623a69f945060ae91d7654bb51575
Joann <joann.kuo@gmail.com>
2012-12-13 23:01:40 -0800

    add title to micropost and styled it

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121011051919) do
+ActiveRecord::Schema.define(:version => 20121214062205) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "title"
   end
 
   add_index "microposts", ["user_id", "created_at"], :name => "index_microposts_on_user_id_and_created_at"
